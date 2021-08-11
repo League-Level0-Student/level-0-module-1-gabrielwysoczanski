046354bfbd8e44b0d9653d42ba1099b08d6a25ae
student <student@ip-172-31-53-169.us-west-2.compute.internal>
2021-08-10 19:58:14 -0700

    Everything is good on these

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,10 +8,12 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+	String Random_People1 = JOptionPane.showInputDialog("Joe is wearing a hat that is cool");
+	String Random_People2 = JOptionPane.showInputDialog("Daniel is wearing a shirt that has a nice smile on it");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-	String Random_People = JOptionPane.showInputDialog("Enter a name.");
+	String Random_People3 = JOptionPane.showInputDialog("Enter a name.");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+String Random_People4 = JOptionPane.showInputDialog("You are really cool");
 	}
 }
 
--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -17,16 +17,22 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
 	
 	// 1. Set a password in a String variable
-	
+	String Password = "NothingMuch";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String Popup = JOptionPane.showInputDialog("Hello");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+String NextPopup = JOptionPane.showInputDialog("If you can guess a password you get to see the 'Secret Message'");
 	// 4. If their guess matches the password, show them the secret message
-
+if(NextPopup.equals(Password)) { 
+JOptionPane.showMessageDialog(null,Popup);
+}else {JOptionPane.showInputDialog("INTRUDER!!");
+	
+}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
+}
 }
