d3484f9553fe9322c1d3687f668b9ac063feea1c
root <root@ed2df8bc7eca>
2021-08-25 03:04:17 +0000

    The Riddler Done and RobotinSpace in the process.

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,22 +2,32 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+JOptionPane.showMessageDialog(null,"Here is a riddle");JOptionPane.showMessageDialog(null,"Brothers and sisters I have none but this man's father is my father's son.")
+;JOptionPane.showInputDialog("Who is the man?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by of 1   
+JOptionPane.showMessageDialog(null,"CORRECT YOU HAVE 1 POINT NOW");
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+JOptionPane.showMessageDialog(null,"IF YOU DIDN'T SAY THE MAN WAS MY SON YOU ARE INCORRECT YOU HAVE A TOTAL OF 0 POINTS");
 		// 6. Add some more riddles
 
+JOptionPane.showMessageDialog(null,"Here is another riddle");
+JOptionPane.showMessageDialog(null,"What is greater than God,"+
+"more evil than the devil,"+
+"the poor have it,"+
+"the rich need it,"+
+"and if you eat it, you'll die?");
+JOptionPane.showInputDialog(null,"What is the answer");
+JOptionPane.showMessageDialog(null,"CORRECT YOU HAVE A TOTAL OF 2 POINTS NOW");
+JOptionPane.showMessageDialog(null,"IF YOU DIDNT SAY NOTHING YOU ARE INCORRECT YOU HAVE A TOTAL OF 0 OR 1 POINTS");
 		// 2. Make a pop up to show the score.
 		
 	}
--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -28,9 +28,13 @@ public class RobotInSpace implements KeyEventDispatcher {
 	private void moveRobot(int keyPressed) throws InterruptedException {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
-
-		// 3. If the up arrow is pressed, move the Robot up the screen.
-
+rob.microMove(100);
+rob.setAngle(90);
+		// 3. If the up arrow is pressed, move the Robot up the screen
+System.out.print(keyPressed);
+if(keyPressed==38) {
+rob.microMove(300);	
+}
 		// 4. If the down arrow is pressed, move the Robot down.
 
 		// 5. If the left arrow is pressed, make the Robot go left.
